6906f62fd71677f2ae87da011a4f93b4418aa622
Isha Paghdal <ishapaghdal@gmail.com>
2024-06-30 16:05:32 +0530

    first

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 const server = http.createServer(app);
 
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: 'https://interview-practice-k9ho.onrender.com',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
